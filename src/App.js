4539bbed104624303d98ad11f8ccdb7db2d59780
GiantAxe <jeszero0@gmail.com>
2019-10-24 01:13:05 -0400

    primer commit base del proyecto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+/*import Panel from "./Panel";
+import getWeb3 from "./getWeb3";
+import DigiCertContract from "./digicert";*/
+
+export class App extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Hola mundo!!!</h1>
+            </div>
+        );
+    }
+}
